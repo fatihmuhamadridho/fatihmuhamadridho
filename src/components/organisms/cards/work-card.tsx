2f6d09cfb0fd0c3b3879db0de4b0a0c02fb98b6b
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2023-07-27 19:13:30 +0700

    fix: 🐛 word-card and work-sections

--- src/components/organisms/cards/work-card.tsx
+++ src/components/organisms/cards/work-card.tsx
@@ -1,23 +1,33 @@
-import { Flex, Image, Text } from "@mantine/core";
+import { Flex, Image, SimpleGrid, Text } from "@mantine/core";
 import React from "react";
+import type { portofolioModelProps } from "@/pages/api/models/portofolioModel";
 
-type WorkCardProps = {
-  reverse?: boolean,
-  title?: string | any;
-  description?: string | any;
-  banner?: string | any;
+interface WorkCardProps extends portofolioModelProps {
+  reverse?: boolean;
 }
 
 const WorkCard = (props: WorkCardProps) => {
-  const { reverse, title, description, banner } = props;
+  const { reverse, title, description, banner, skills } = props;
 
   return (
-    <Flex direction={reverse ? "row-reverse" : "row"}>
-      <Image src={banner || "https://dummyimage.com/250/ffffff/000000"} alt="work-image" />
-      <div>
+    <Flex w={"100%"} direction={reverse ? "row-reverse" : "row"}>
+      <Flex w={"100%"} p={48}>
+        <Image
+          src={banner || "https://dummyimage.com/250/ffffff/000000"}
+          alt="work-image"
+          width={"auto"}
+          height={384}
+        />
+      </Flex>
+      <Flex w={"100%"} p={48} direction={"column"} gap={24}>
         <Text>{title || "<title>"}</Text>
         <Text>{description || "<description>"}</Text>
-      </div>
+        <SimpleGrid cols={10}>
+          {skills.map((skillData, skillIndex: number) => (
+            <Text key={skillIndex}>{skillData}</Text>
+          ))}
+        </SimpleGrid>
+      </Flex>
     </Flex>
   );
 };
--- src/components/organisms/sections/work-section.tsx
+++ src/components/organisms/sections/work-section.tsx
@@ -2,22 +2,24 @@ import { Badge, Center, Flex, Text } from "@mantine/core";
 import React from "react";
 import WorkCard from "../cards/work-card";
 import { useGetAllPortofolio } from "@/services/portofolioService";
+import type { portofolioModelProps } from "@/pages/api/models/portofolioModel";
 
 const WorkSection = () => {
-  const { data: portofolioData }: { data: any[] } = useGetAllPortofolio();
+  const { data: portofolioData }: { data: Array<portofolioModelProps> } = useGetAllPortofolio();
 
   return (
     <Flex direction={"column"} py={96} px={80 + 32} gap={48}>
       <Center className="flex-col gap-4">
         <Badge>work</Badge>
         <Text>Some of the noteworthy projects I have built:</Text>
-        {portofolioData?.map((portoData: any, portoIndex: number) => (
+        {portofolioData?.map((portoData, portoIndex: number) => (
           <WorkCard
             key={portoIndex}
             reverse={portoIndex % 2 === 1 ? true : false}
             title={portoData.title}
             description={portoData.description}
             banner={portoData.banner}
+            skills={portoData.skills}
           />
         ))}
       </Center>
