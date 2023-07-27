47080b36305194945dc9b4a10bfcc01b3b3e01ff
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2023-07-27 18:44:38 +0700

    feat: 🎸 setup react query service for porto and base

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        cacheTime: 5 * 60 * 1000,
+      },
+    },
+  });
+
   return (
-    <MantineProvider>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </QueryClientProvider>
   );
 }
