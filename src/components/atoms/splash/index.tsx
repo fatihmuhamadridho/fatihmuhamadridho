148078cea8e7e1373eabadefda75a1cc2e06ce69
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2023-07-27 19:01:14 +0700

    feat: 🎸 loading splash screen

---
+++ src/components/atoms/splash/index.tsx
@@ -0,0 +1,5 @@
+const Splash = () => {
+  return <div className="w-[100vw] h-[100vh] flex items-center justify-center">...loading</div>;
+};
+
+export default Splash;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { useState } from "react";
+import Splash from "@/components/atoms/splash";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  setTimeout(() => {
+    setLoading(false);
+  }, 500);
+
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -16,7 +24,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <MantineProvider>
-        <Component {...pageProps} />
+        {loading && <Splash />}
+        {!loading && <Component {...pageProps} />}
       </MantineProvider>
     </QueryClientProvider>
   );
