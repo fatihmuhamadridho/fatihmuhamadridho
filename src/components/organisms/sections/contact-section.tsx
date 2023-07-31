317e93352be2f702cf2e6625ece8a9b67acf95c5
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2023-07-31 09:28:12 +0700

    feat: 🎸 contact section slicing

--- src/components/organisms/sections/contact-section.tsx
+++ src/components/organisms/sections/contact-section.tsx
@@ -1,11 +1,57 @@
-import { Flex } from "@mantine/core";
+import { Badge, Center, Flex, Paper, Text, TypographyStylesProvider } from "@mantine/core";
+import {
+  IconBrandFigma,
+  IconBrandGithub,
+  IconBrandTwitter,
+  IconCopy,
+  IconMail,
+  IconPhone,
+} from "@tabler/icons-react";
 import React from "react";
 
 const ContactSection = () => {
   return (
-    <Flex py={96} px={80 + 32} gap={48}>
-      <div>ContactSection</div>
-    </Flex>
+    <Paper py={96} px={80 + 32} bg={"#111827"}>
+      <TypographyStylesProvider className="text-white">
+        <Center p={0}>
+          <Flex direction={"column"} align={"center"} maw={600} w={"100%"} gap={48}>
+            <Badge w={"auto"} py={4} px={20} className="text-[#D1D5DB] normal-case" bg={"#374151"}>
+              Get in touch
+            </Badge>
+            <Text fz={20} fw={400} lh={"28px"} color="#D1D5DB" ta={"center"}>
+              What&apos;s next? Feel free to reach out to me if you&apos;re looking for a developer,
+              have a query, or simply want to connect.
+            </Text>
+            <Flex direction={"column"} align={"center"} gap={16}>
+              <Flex align={"center"} gap={20}>
+                <IconMail size={32} />
+                <Text fz={36} fw={600} lh={"40px"}>
+                  fatihmuhamadridho1@gmail.com
+                </Text>
+                <IconCopy size={32} />
+              </Flex>
+              <Flex align={"center"} gap={20}>
+                <IconPhone size={32} />
+                <Text fz={36} fw={600} lh={"40px"}>
+                  +62 82110797472
+                </Text>
+                <IconCopy size={32} />
+              </Flex>
+            </Flex>
+            <Flex direction={"column"} align={"center"} gap={8}>
+              <Text fz={16} fw={400} lh={"24px"}>
+                You may also find me on these platforms!
+              </Text>
+              <Flex>
+                <IconBrandGithub className="p-[6px]" size={24 + 12} />
+                <IconBrandTwitter className="p-[6px]" size={24 + 12} />
+                <IconBrandFigma className="p-[6px]" size={24 + 12} />
+              </Flex>
+            </Flex>
+          </Flex>
+        </Center>
+      </TypographyStylesProvider>
+    </Paper>
   );
 };
 
