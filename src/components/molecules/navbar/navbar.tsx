1ca99b6b3372a2dfa5f8827a002b45afd58ca5d7
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2023-08-05 09:55:37 +0700

    feat: 🎸 handle donwload my resume.pdf

--- src/components/molecules/navbar/navbar.tsx
+++ src/components/molecules/navbar/navbar.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Divider, Flex, Text, TypographyStylesProvider, UnstyledButton } from "@mantine/core";
-import { IconMoonStars } from "@tabler/icons-react";
 import { useSectionContext } from "@/contexts/section/SectionContext";
+// import { IconMoonStars } from "@tabler/icons-react";
 
 const Navbar = () => {
   const { sections } = useSectionContext();
+
+  const handleDownload = () => {
+    const downloadLink = document.createElement("a");
+    downloadLink.href = "/files/Fatih Muhamad Ridho-resume.pdf";
+    downloadLink.download = "Fatih_Muhamad_Ridho-resume.pdf";
+    downloadLink.click();
+  };
 
   return (
     <Flex className="sticky top-0 bg-black z-50" direction={"column"} p={16} px={80}>
@@ -16,7 +23,7 @@ const Navbar = () => {
           lh={"36px"}
           color="#F9FAFB"
           onClick={() => window.scrollTo({ top: 0, left: 0, behavior: "smooth" })}
-        >{`<SS />`}</Text>
+        >{`Fatih M Ridho`}</Text>
         <TypographyStylesProvider className="text-white" fw={500} lh={"24px"}>
           <Flex gap={24} align={"center"}>
             <Text className="cursor-pointer" onClick={() => sections.scrollIntoAbout()}>
@@ -38,6 +45,7 @@ const Navbar = () => {
                 py={6}
                 px={16}
                 className="text-black bg-white rounded-[12px] active:bg-black active:text-white"
+                onClick={handleDownload}
               >
                 Download CV
               </UnstyledButton>
