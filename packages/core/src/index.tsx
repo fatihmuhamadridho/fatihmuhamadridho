8e34c96fa23f971d7cf70f5ed6d78742307c6313
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2024-01-30 16:28:35 +0700

    test: 💍 package core

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Text as TextCore } from "@mantine/core";
 
-export const Text = () => (
-  <TextCore p={16} bg={"black"} c={"white"}>
+export const Text: React.FC = () => (
+  <TextCore p={16} bg={"black"} color={"white"}>
     test
   </TextCore>
 );
