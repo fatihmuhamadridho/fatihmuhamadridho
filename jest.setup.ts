7fe6d36a534c500ec698bc3b89b71c551dea6f6f
fatihmuhamadridho <fatih.ridho@salt.co.id>
2025-09-06 23:35:05 +0700

    fix: 🐛 error build bcs jest.setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import '@testing-library/jest-dom';
-import { TextEncoder } from 'util';
+import { TextEncoder, TextDecoder } from 'util';
 
-global.TextEncoder = TextEncoder;
-global.fetch = () =>
+(global as any).TextEncoder = TextEncoder;
+(global as any).TextDecoder = TextDecoder;
+(global as any).fetch = () =>
   Promise.resolve({
     json: () => Promise.resolve([]),
-  }) as any;
+  });
